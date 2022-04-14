5276c60a284c78f844983554d739755bd48a730d
gray-code <koheiishido@gmail.com>
2022-04-14 17:34:19 +0900

    [new] vol2

---
+++ vol2/contactManager/contactManager.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Contact Manager
+Description: お問い合わせを管理するためのプラグイン
+Version: 0.1
+Author: GRAYCODE
+Author URI: https://gray-code.com
+Licence: GPL v2 or later
+Licence URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
