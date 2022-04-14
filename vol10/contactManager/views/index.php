140d55e876f4e6cb65b8b46f413b9bb1db241df5
gray-code <koheiishido@gmail.com>
2022-04-14 17:38:30 +0900

    [new] vol10

---
+++ vol10/contactManager/views/index.php
@@ -0,0 +1,32 @@
+<table class="data_table">
+	<tr>
+		<th>日時</th><th>お名前</th><th>メールアドレス</th><th>お問い合わせ内容</th><th></th>
+	</tr>
+	<tr>
+		<td>2022-04-07 00:00:00</td>
+		<td>テスト太郎</td>
+		<td>taro@gray-code.com</td>
+		<td>テストです。</td>
+		<td>
+			<a href="#">編集</a>&nbsp;&nbsp;<a href="#">削除</a>
+		</td>
+	</tr>
+	<tr>
+		<td>2022-04-07 01:01:00</td>
+		<td>テスト花子</td>
+		<td>hanako@gray-code.com</td>
+		<td>テストです。</td>
+		<td>
+			<a href="#">編集</a>&nbsp;&nbsp;<a href="#">削除</a>
+		</td>
+	</tr>
+	<tr>
+		<td>2022-04-07 02:02:00</td>
+		<td>テスト健太</td>
+		<td>kenta@gray-code.com</td>
+		<td>テストです。</td>
+		<td>
+			<a href="#">編集</a>&nbsp;&nbsp;<a href="#">削除</a>
+		</td>
+	</tr>
+</table>
